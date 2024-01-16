9a3bbae0d0765686f3333a5c62d57d7f79ba48a2
Hữu Tường <hunter.19oct@gmail.com>
2024-01-16 16:18:17 +0700

    Update

--- BÀI 1/src/HoTenDiem.java
+++ BÀI 1/src/HoTenDiem.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class HoTenDiem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Họ Và Tên:");
+		String hoten = scanner.nextLine();
+		System.out.print("Điêm:");
+		Double diem = scanner.nextDouble();
+		System.out.printf ("%s: &f",hoten,diem);
 	}
 
 }
