3b5b3133f5cfb78a3bab38eece60520477158124
Hữu Tường <hunter.19oct@gmail.com>
2024-01-16 09:32:46 +0700

    Update Main.java

--- TenTuoi/src/Main.java
+++ TenTuoi/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int namsinh;
+		String hoten;
+		Scanner banphim = new Scanner(System.in);
+		hoten = banphim.nextLine();
+		namsinh = banphim.nextInt();
+		int tuoi = 2024 - namsinh;
+		System.out.printf("%s Năm nay %d Tuổi ",hoten,tuoi);
 	}
 
 }
