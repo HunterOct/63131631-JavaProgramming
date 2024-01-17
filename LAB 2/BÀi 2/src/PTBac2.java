6565513515f14352db79db2164e5df4311eac5ff
Hữu Tường <hunter.19oct@gmail.com>
2024-01-17 19:36:32 +0700

    Update PTBac2.java

--- LAB 2/BÀi 2/src/PTBac2.java
+++ LAB 2/BÀi 2/src/PTBac2.java
@@ -1,9 +1,25 @@
-
+import java.util.Scanner;
 public class PTBac2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập a:");
+		double a = scanner.nextDouble();
+		System.out.print("Nhập b:");
+		double b = scanner.nextDouble();
+		System.out.print("Nhập c:");
+		double c = scanner.nextDouble();
+		if (a==0)
+		{
+			double x = -b/a;
+			System.out.printf("Nghiệm Của Phương trình bậc 1:%.1f",x);
+		}
+		else
+		{
+			
+		}
+		
+			
 	}
 
 }
