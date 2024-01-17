8dc6efc305ae2384c120b044d03746dfa84d1318
Hữu Tường <hunter.19oct@gmail.com>
2024-01-17 18:32:15 +0700

    Update ThetichHCN.java

--- BÀI 3/src/ThetichHCN.java
+++ BÀI 3/src/ThetichHCN.java
@@ -1,6 +1,10 @@
-
+import java.util.Scanner;
 public class ThetichHCN {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập Cạnh Của khối lập Phương:");
+		int canh = scanner.nextInt();
+		int thetich = Math.pow(canh,3);
+		System.out.printf("Thể tích của khối lập phương: %d",thetich);
 	}
 }
