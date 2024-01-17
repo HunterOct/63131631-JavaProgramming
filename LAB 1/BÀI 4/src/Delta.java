fd66d601660e2cc82023a1f7be686d164a6452b3
Hữu Tường <hunter.19oct@gmail.com>
2024-01-17 18:59:59 +0700

    Update Delta.java

--- LAB 1/BÀI 4/src/Delta.java
+++ LAB 1/BÀI 4/src/Delta.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Delta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println("Nhập hệ số a: ");
+        double a = scanner.nextDouble();
 
-	}
+        System.out.println("Nhập hệ số b: ");
+        double b = scanner.nextDouble();
 
+        System.out.println("Nhập hệ số c: ");
+        double c = scanner.nextDouble();
+
+        double delta = Math.pow(b, 2) - 4*a*c;
+
+        if (delta < 0) {
+            System.out.println("Phương trình vô nghiệm");
+        } else {
+            System.out.println("Căn của Delta là: " + Math.sqrt(delta));
+        }
+    }
 }
