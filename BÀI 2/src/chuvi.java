ae739e14b449397cfaf6594577e66de33b8ab2ea
Hữu Tường <hunter.19oct@gmail.com>
2024-01-17 18:16:15 +0700

    Update chuvi.java

--- BÀI 2/src/chuvi.java
+++ BÀI 2/src/chuvi.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class chuvi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int chieudai,chieurong,cvi,dientich,canhmin;
+		System.out.print("Nhập Chiều Dài :");
+		chieudai = scanner.nextInt();
+		chieurong = scanner.nextInt();
+		canhmin = Math.min(chieudai, chieurong);
 	}
 
 }
