525331caaa39191404d9adf9d0997a1dd79475d8
Hữu Tường <hunter.19oct@gmail.com>
2024-01-17 18:20:24 +0700

    Update chuvi.java

--- BÀI 2/src/chuvi.java
+++ BÀI 2/src/chuvi.java
@@ -7,8 +7,14 @@ public class chuvi {
 		int chieudai,chieurong,cvi,dientich,canhmin;
 		System.out.print("Nhập Chiều Dài :");
 		chieudai = scanner.nextInt();
+		System.out.print("Nhập Chiều Rộng:");
 		chieurong = scanner.nextInt();
+		dientich = chieudai * chieurong;
+		cvi = (chieudai + chieurong)*2;
 		canhmin = Math.min(chieudai, chieurong);
+		System.out.printf("Diện Tích :%d",dientich);
+		System.out.printf("Chu Vi :%d",cvi);
+		System.out.printf("Cạnh Nhỏ Nhất :%d",canhmin);
 	}
 
 }
