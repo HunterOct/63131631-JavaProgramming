3224c40b27edef52ae42df64dc200cae068f5daa
Hữu Tường <hunter.19oct@gmail.com>
2024-01-17 19:29:48 +0700

    Update PTBac1.java

--- LAB 2/BÀI 1/src/PTBac1.java
+++ LAB 2/BÀI 1/src/PTBac1.java
@@ -3,9 +3,9 @@ public class PTBac1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Nhập a:");
-		float a = scanner.nextFloat();
+		double a = scanner.nextDouble();
 		System.out.print("Nhập b:");
-		float b = scanner.nextFloat();
+		double b = scanner.nextDouble();
 		if(a == 0)
 		{
 			if(b == 0)
@@ -17,10 +17,7 @@ public class PTBac1 {
 				System.out.print("Phương trình vô nghiệm !");
 			}
 		}
-		else
-		{
-			float x = -b/a;
-			System.out.printf("Nghiệm Của Chương trình :%.f",x);
-		}
+		double x = -b/a;
+		System.out.printf("Nghiệm Của Chương trình :%.1f",x);
 	}
 }
