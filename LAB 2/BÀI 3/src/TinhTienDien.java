9af9c807d34a3677ff19a24b23d76861d33ed579
Hữu Tường <hunter.19oct@gmail.com>
2024-01-18 18:51:11 +0700

    Update TinhTienDien.java

--- LAB 2/BÀI 3/src/TinhTienDien.java
+++ LAB 2/BÀI 3/src/TinhTienDien.java
@@ -1,9 +1,19 @@
-
+import java.util.Scanner;
 public class TinhTienDien {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int sodien;
+		int giadien = 0;
+		int tiendien;
+		System.out.print("Nhập Số Điện Sử Dụng:");
+		sodien = scanner.nextInt();
+		if (sodien >= 0 && sodien <= 50)
+			giadien = 1000;
+			if (sodien > 50)
+					giadien = 1200;
+		tiendien = sodien*giadien;
+		System.out.printf("Tiền Điện:%d",tiendien);
 	}
 
 }
