3c883cca09386d21548c7c648992d721525a7791
Hữu Tường <hunter.19oct@gmail.com>
2024-01-23 20:52:15 +0700

    Update NhapMangHSSV.java

--- LAB 3/BÀI 4/src/NhapMangHSSV.java
+++ LAB 3/BÀI 4/src/NhapMangHSSV.java
@@ -1,9 +1,28 @@
-
+import java.util.Scanner;
 public class NhapMangHSSV {
+	
+	public static void NhapHoTen(String[] hoTen,int diem[])
+	{
+		Scanner scanner = new Scanner(System.in);
+		int n = 0;
+		do
+		{
+			System.out.print("Nhập Số Lượng Sinh Viên:");
+			 n = scanner.nextInt();
+		}while(n<=0);
+		hoTen = new String[n];
+		diem = new int[n];
+		for (int i = 0 ;i<n;i++)
+		{
+			System.out.print("Nhập Tên Sinh Viên %d");
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n = 0;
+		String[] hoTenSv = new String[n];
+		int diemSv[] = new int[n];
+		NhapHoTen(hoTenSv,diemSv);
 	}
 
 }
