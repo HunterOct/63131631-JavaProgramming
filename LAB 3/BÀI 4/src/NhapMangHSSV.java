704a31773fef7d4f3c8c33956d42f4497f3568bd
Hữu Tường <hunter.19oct@gmail.com>
2024-01-23 21:20:35 +0700

    Update NhapMangHSSV.java

--- LAB 3/BÀI 4/src/NhapMangHSSV.java
+++ LAB 3/BÀI 4/src/NhapMangHSSV.java
@@ -12,7 +12,23 @@ public class NhapMangHSSV {
             scanner.nextLine();
         }
     }
+    public static void HocLuc(double diem[])
+    {	
+    	for (int i = 0 ;i< 10;i++)
+    	{
+    		if (diem[i] < 5)
+                System.out.print("/Học Lực Yếu");
+    		else if (diem[i] >= 5 && diem[i]< 6.5)
+                System.out.print("/Học Lực Trung Bình");
+    		else if (diem[i] >= 6.5 && diem[i]< 7.5)
+                System.out.print("/Học Lực Khá");
+    		else if (diem[i] >= 7.5 && diem[i]< 9)
+                System.out.print("/Học Lực Giỏi");
+    		else if (diem[i] > 9)
+                System.out.print("/Học Lực Xuất Sắc");
+    	}
 
+    }
     public static void main(String[] args) {
         int n = 0;
         Scanner scanner = new Scanner(System.in);
@@ -23,5 +39,6 @@ public class NhapMangHSSV {
         String[] hoTenSv = new String[n];
         double diemSv[] = new double[n];
         NhapHoTen(hoTenSv, diemSv, n);
+        HocLuc(diemSv);
     }
 }
