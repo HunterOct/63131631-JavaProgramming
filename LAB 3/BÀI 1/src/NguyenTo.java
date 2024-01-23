e3c1baa666d383ae41ca0d7b552985611bffa43a
Hữu Tường <hunter.19oct@gmail.com>
2024-01-23 19:04:06 +0700

    Update NguyenTo.java

--- LAB 3/BÀI 1/src/NguyenTo.java
+++ LAB 3/BÀI 1/src/NguyenTo.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
+
 public class NguyenTo {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n;
-		Boolean nt  = true;
-		do
-		{
-			System.out.print("Nhập Một Số Nguyên:");
-			n = scanner.nextInt();\
-		}while(n < 0);
-		for (int i=2;i<n-1;i++)
-		{
-			if(n%i==0)
-			{
-				nt = false;
-				break;
-			}
-			i++;
-			if(nt = true)
-				System.out.print("Đây là số nguyên tố !!!");
-		}
-		
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        boolean nt = true;
 
+        do {
+            System.out.print("Nhập Một Số Nguyên:");
+            n = scanner.nextInt();
+        } while (n <= 0);
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                nt = false;
+                break;
+            }
+        }
+
+        if (nt && n > 1) {
+            System.out.println("Đây là số nguyên tố !!!");
+        } else {
+            System.out.println("Đây không phải là số nguyên tố !!!");
+        }
+    }
 }
