30aa348e7aaf0b9f09a2b774ed04e6e49752c05a
Hữu Tường <hunter.19oct@gmail.com>
2024-01-23 19:12:46 +0700

    Update BangCuuChuong.java

--- LAB 3/BÀI 2/src/BangCuuChuong.java
+++ LAB 3/BÀI 2/src/BangCuuChuong.java
@@ -1,9 +1,14 @@
-
+import java.util.Scanner;
 public class BangCuuChuong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int n;
+		do 
+		{
+			System.out.print("Nhập Bảng Cửu Chương Cần in:");
+			n = scanner.nextInt();
+		}while(n <= 0 || n >= 10);
 
 	}
-
 }
