c87b9cec89d82fdeb240b4212724b425915cee91
Hữu Tường <hunter.19oct@gmail.com>
2024-01-23 21:06:14 +0700

    Update NhapMangHSSV.java

--- LAB 3/BÀI 4/src/NhapMangHSSV.java
+++ LAB 3/BÀI 4/src/NhapMangHSSV.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
+
 public class NhapMangHSSV {
-	
-	public static void NhapHoTen(String[] hoTen,int diem[])
-	{
-		Scanner scanner = new Scanner(System.in);
-		int n = 0;
-		do
-		{
-			System.out.print("Nhập Số Lượng Sinh Viên:");
-			 n = scanner.nextInt();
-		}while(n<=0);
 
-		for (int i = 0 ;i<n;i++)
-		{
-			System.out.printf("Nhập Tên Sinh Viên Thứ %d:",i+1);
-            scanner.nextLine();
-			hoTen[i] = scanner.nextLine();
-		}
-	}
+    public static void NhapHoTen(String[] hoTen, double diem[], int n) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Nhập Tên Sinh Viên Thứ %d:", i + 1);
+            hoTen[i] = scanner.nextLine();
+            System.out.printf("Nhập Điểm Sinh Viên Thứ %d:", i + 1);
+            diem[i] = scanner.nextDouble();
+        }
+    }
 
-	public static void main(String[] args) {
-		int n = 0;
-		String[] hoTenSv = new String[n];
-		int diemSv[] = new int[n];
-		NhapHoTen(hoTenSv,diemSv);
-	}
+    public static void main(String[] args) {
+        int n = 0;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Nhập Số Lượng Sinh Viên:");
+            n = scanner.nextInt();
+        } while (n <= 0);
 
+        String[] hoTenSv = new String[n];
+        double diemSv[] = new double[n];
+        NhapHoTen(hoTenSv, diemSv, n);
+    }
 }
