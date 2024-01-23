1ce7f322f858966a365286ed8ba91b6c0fda238c
Hữu Tường <hunter.19oct@gmail.com>
2024-01-23 19:34:53 +0700

    Update NhapMangXuatMang.java

--- LAB 3/BÀI 3/src/NhapMangXuatMang.java
+++ LAB 3/BÀI 3/src/NhapMangXuatMang.java
@@ -1,4 +1,15 @@
-
+import java.util.Scanner;
 public class NhapMangXuatMang {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập Độ Dài Mảng A:");
+		int n = scanner.nextInt();
+		int A[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			System.out.printf("Nhập A[%d]:",i+1);
+			A[i] = scanner.nextInt();
+		}
+		
+	}
 }
