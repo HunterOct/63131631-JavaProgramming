452e03277404f6e9b84289a864043763abaa02b3
Hữu Tường <hunter.19oct@gmail.com>
2024-01-23 19:21:02 +0700

    Update BangCuuChuong.java

--- LAB 3/BÀI 2/src/BangCuuChuong.java
+++ LAB 3/BÀI 2/src/BangCuuChuong.java
@@ -11,5 +11,8 @@ public class BangCuuChuong {
 		}while(n <= 0 || n >= 10);
 		for (int i = 1;i <= 10;i++)
 			System.out.printf("\n%d x %d = %d",n,i,n*i);
+		for (int i = 1;i<=9;i++)
+			for (int j = 1;j<=10;j++)
+				System.out.printf("\n%d x %d = %d",i,j,i*j);
 	}
 }
