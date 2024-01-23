34e764016068a016ea871d741a42e0897ef9a25d
Hữu Tường <hunter.19oct@gmail.com>
2024-01-23 19:02:58 +0700

    Update NguyenTo.java

--- LAB 3/BÀI 1/src/NguyenTo.java
+++ LAB 3/BÀI 1/src/NguyenTo.java
@@ -1,9 +1,27 @@
-
+import java.util.Scanner;
 public class NguyenTo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int n;
+		Boolean nt  = true;
+		do
+		{
+			System.out.print("Nhập Một Số Nguyên:");
+			n = scanner.nextInt();\
+		}while(n < 0);
+		for (int i=2;i<n-1;i++)
+		{
+			if(n%i==0)
+			{
+				nt = false;
+				break;
+			}
+			i++;
+			if(nt = true)
+				System.out.print("Đây là số nguyên tố !!!");
+		}
+		
 	}
 
 }
