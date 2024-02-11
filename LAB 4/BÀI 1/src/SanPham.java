d74cf71f26df8a317127edaea93d63a0a083ae59
Hữu Tường <hunter.19oct@gmail.com>
2024-02-11 15:52:33 +0700

    Update SanPham.java

--- LAB 4/BÀI 1/src/SanPham.java
+++ LAB 4/BÀI 1/src/SanPham.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class SanPham {
 	
 	public static double getThueNhapKhau(double gia)
@@ -9,25 +11,29 @@ public class SanPham {
 	{
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Nhập Tên Sản Phẩm:");
-		String ten = scanner.next();
+		String ten = scanner.nextLine();
 		System.out.print("Nhập Giá Sản Phẩm:");
 		double gia = scanner.nextDouble();
 		System.out.print("Nhập Phần Trăm Giảm Giá:");
 		double giamGia = scanner.nextDouble();
+		Xuat(ten,gia,giamGia);
 	}
 	public static void Xuat(String ten ,double gia,double giamgia) {
-		System.out.printf("Tên Sản Phẩm: %s", ten);
-		System.out.printf("\nĐơn Giá: %.1f", gia);
+		DecimalFormat dinhdang = new DecimalFormat("###,###,###.##");
+		String dinhDangGia = dinhdang.format(gia);
+		String dinhDangThue = dinhdang.format(getThueNhapKhau(gia));
+		System.out.printf("----------------------------------");
+		System.out.printf("\nTên Sản Phẩm: %s", ten);
+		System.out.printf("\nĐơn Giá: %s", dinhDangGia);
 		System.out.printf("\nGiảm giá: %.1f %%", giamgia);
-		System.out.printf("\nThuế Nhập Khẩu: %.1f", getThueNhapKhau(gia));
+		System.out.printf("\nThuế Nhập Khẩu: %s", dinhDangThue);
+		System.out.printf("\n----------------------------------");
+
 
 	}
 	public static void main(String[] args) {
-		String ten = null ;
-		double gia = 0;
-		double giamGia = 0;
 		Nhap();
-		Xuat(ten,gia,giamGia);
+
 	}
 	
 
