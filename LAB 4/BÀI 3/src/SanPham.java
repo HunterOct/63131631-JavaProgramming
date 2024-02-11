a0ae7d583da58e0f5a36c2018958a6b708d1de26
Hữu Tường <hunter.19oct@gmail.com>
2024-02-11 16:38:44 +0700

    Update SanPham.java

--- LAB 4/BÀI 3/src/SanPham.java
+++ LAB 4/BÀI 3/src/SanPham.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class SanPham {
+	
+	private static String ten;
+	private static double gia;
+	private static double giamGia;
+	
+	public SanPham(String ten,double gia,double giamGia)
+	{
+		this.ten = ten;
+		this.gia = gia;
+		this.giamGia = giamGia;
+	}
+	public SanPham(String ten,double gia)
+	{
+		this(ten,gia,0);
+	}
 	public static double getThueNhapKhau(double gia)
 	{
 		return gia * 0.1;
@@ -10,11 +25,11 @@ public class SanPham {
 	{
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("\nNhập Tên Sản Phẩm:");
-		String ten = scanner.nextLine();
+		ten = scanner.nextLine();
 		System.out.print("Nhập Giá Sản Phẩm:");
-		double gia = scanner.nextDouble();
+		gia = scanner.nextDouble();
 		System.out.print("Nhập Phần Trăm Giảm Giá:");
-		double giamGia = scanner.nextDouble();
+		giamGia = scanner.nextDouble();
 		Xuat(ten,gia,giamGia);
 	}
 	public static void Xuat(String ten ,double gia,double giamgia) {
@@ -29,8 +44,8 @@ public class SanPham {
 		System.out.printf("\n----------------------------------");
 	}
 	public static void main(String[] args) {
-		SanPham sp1 = new SanPham();
-		SanPham Sp2 = new SanPham();
+		SanPham sp1 = new SanPham(ten,gia,giamGia);
+		SanPham Sp2 = new SanPham(ten,gia);
 		System.out.printf("            Nhập Thông Tin Sản Phẩm 1");
 		sp1.Nhap();
 		System.out.printf("\n            Nhập Thông Tin Sản Phẩm 2");
