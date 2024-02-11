5d7c5324fe610e0d5eb6386917f1f3f7240f9c1c
Hữu Tường <hunter.19oct@gmail.com>
2024-02-11 15:30:09 +0700

    Update SanPham.java

--- LAB 4/BÀI 1/src/SanPham.java
+++ LAB 4/BÀI 1/src/SanPham.java
@@ -14,15 +14,16 @@ public class SanPham {
 	}
 	public static void Xuat(String ten ,double gia,double giamgia) {
 		System.out.printf("Tên Sản Phẩm: %s", ten);
-		System.out.printf("Đơn Giá: %.1f", gia);
-		System.out.printf("Giảm giá: %.1f %", giamgia);
-		System.out.printf("Thuế Nhập Khẩu: %.1f", getThueNhapKhau(gia));
+		System.out.printf("\nĐơn Giá: %.1f", gia);
+		System.out.printf("\nGiảm giá: %.1f %", giamgia);
+		System.out.printf("\nThuế Nhập Khẩu: %.1f", getThueNhapKhau(gia));
 
 	}
 	public static void main(String[] args) {
 		String ten;
 		double gia;
 		double giamGia;
+		Nhap(String ten,double gia,double giamgia)
 	}
 	
 
