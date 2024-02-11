7d886e91327e816ff62ee4eacfbaa25e532f1eb9
Hữu Tường <hunter.19oct@gmail.com>
2024-02-11 15:29:15 +0700

    Original

---
+++ LAB 4/BÀI 1/src/SanPham.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class SanPham {
+	
+	public static double getThueNhapKhau(double gia)
+	{
+		return gia * 0.1;
+	}
+	public static void Nhap(String ten,double gia,double giamgia)
+	{
+		Scanner scanner = new Scanner(System.in);
+		ten = scanner.next();
+		gia = scanner.nextDouble();
+		giamgia = scanner.nextDouble();
+	}
+	public static void Xuat(String ten ,double gia,double giamgia) {
+		System.out.printf("Tên Sản Phẩm: %s", ten);
+		System.out.printf("Đơn Giá: %.1f", gia);
+		System.out.printf("Giảm giá: %.1f %", giamgia);
+		System.out.printf("Thuế Nhập Khẩu: %.1f", getThueNhapKhau(gia));
+
+	}
+	public static void main(String[] args) {
+		String ten;
+		double gia;
+		double giamGia;
+	}
+	
+
+}
