c3f5c1da936d3db4e30da278c45c47abddecaae8
Hữu Tường <hunter.19oct@gmail.com>
2024-02-19 16:05:02 +0700

    Update HOTEN.java

--- LAB 5/BAI 2/src/HOTEN.java
+++ LAB 5/BAI 2/src/HOTEN.java
@@ -1,22 +1,46 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class HOTEN {
-
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> danhsach = new ArrayList<>();
 		while (true)
 		{
-			Scanner scanner = new Scanner(System.in);
-			System.out.print("1. Nhập danh sách họ và tên");
+			System.out.print("\n1. Nhập danh sách họ và tên");
 			System.out.print("\n2. Xuất danh sách vừa nhập");
 			System.out.print("\n3. Xuất danh sách ngẫu nhiên");
 			System.out.print("\n4. Sắp xếp giảm dần và xuất danh sách");
 			System.out.print("\n5. Tìm và xóa họ tên nhập từ bàn phím");
-			System.out.print("\n6. Kết thúc");
+			System.out.print("\n6. Kết thúc\n");
 			int n = scanner.nextInt();
 			switch (n)
 			{
 				case 1:
+					 while (true) {
+				            System.out.print("Nhập Họ Tên: ");
+				            String them = scanner.next();
+				            danhsach.add(them);
+				            System.out.print("Nhập thêm (Y/N)? ");
+				            scanner.nextLine(); // consume the newline character
+				            if (scanner.nextLine().equalsIgnoreCase("N")) {
+				                break;
+				            }
+				        }
 					break;
 				case 2:
+					System.out.print("Danh Sách Vừa Nhập:\n");
+						if (danhsach.isEmpty())
+						{
+							System.out.print("Chưa Có Tên Nào Trong Danh Sách !!!\n");
+							break;
+						}
+						else
+						{
+							for (String inDanhSach : danhsach)
+							{
+							System.out.printf(inDanhSach);
+							}
+						}
 					break;
 				case 3:
 					break;
@@ -28,6 +52,6 @@ public class HOTEN {
 					break;
 			}
 		}
-		}
+	}
 
 }
