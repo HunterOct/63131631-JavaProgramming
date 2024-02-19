796666efc5e872f671aac89c61a0f5203768013c
Hữu Tường <hunter.19oct@gmail.com>
2024-02-19 15:48:09 +0700

    Update HOTEN.java

--- LAB 5/BAI 2/src/HOTEN.java
+++ LAB 5/BAI 2/src/HOTEN.java
@@ -1,9 +1,33 @@
-
+import java.util.Scanner;
 public class HOTEN {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		while (true)
+		{
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("1. Nhập danh sách họ và tên");
+			System.out.print("\n2. Xuất danh sách vừa nhập");
+			System.out.print("\n3. Xuất danh sách ngẫu nhiên");
+			System.out.print("\n4. Sắp xếp giảm dần và xuất danh sách");
+			System.out.print("\n5. Tìm và xóa họ tên nhập từ bàn phím");
+			System.out.print("\n6. Kết thúc");
+			int n = scanner.nextInt();
+			switch (n)
+			{
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+			}
+		}
+		}
 
 }
