d4b0d68237165e3f8651d6737b6a136e2ab5e139
Hữu Tường <hunter.19oct@gmail.com>
2024-02-22 08:51:32 +0700

    Update

--- SinhVien/src/bt1/Main.java
+++
@@ -1,8 +0,0 @@
-package bt1;
-
-public class Main {
-    public static void main(String[] args){
-        SinhVien63 sv1 = new SinhVien63("Hữu Tường","Khánh Hòa",20,150,1_000_000);
-        System.out.println(sv1.toString());
-    }
-}
