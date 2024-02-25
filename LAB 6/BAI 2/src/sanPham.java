de5b03194930634d77f18dfa3d2b8b62711b541a
Hữu Tường <hunter.19oct@gmail.com>
2024-02-25 19:16:36 +0700

    Update sanPham.java

--- LAB 6/BAI 2/src/sanPham.java
+++ LAB 6/BAI 2/src/sanPham.java
@@ -3,7 +3,9 @@ public class sanPham {
 	public void nhap(String tenSp,String hang,double donGia) {
 		
 	}
-
+	public void xuat(String tenSp,String hang,double donGia) {
+		
+	}
 	public static void main(String[] args) {
 		String tenSp;
 		String hang;
