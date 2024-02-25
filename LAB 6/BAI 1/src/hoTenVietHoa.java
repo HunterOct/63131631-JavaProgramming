3fce94889cf60f0db4bb2cbddef0f6fdcf8fdae3
Hữu Tường <hunter.19oct@gmail.com>
2024-02-25 19:13:24 +0700

    Update hoTenVietHoa.java

--- LAB 6/BAI 1/src/hoTenVietHoa.java
+++ LAB 6/BAI 1/src/hoTenVietHoa.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class hoTenVietHoa {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập họ và tên: ");
+        String fullName = scanner.nextLine().trim();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int firstSpaceIndex = fullName.indexOf(' ');
+        int lastSpaceIndex = fullName.lastIndexOf(' ');
 
-	}
+        String ho = fullName.substring(0, firstSpaceIndex).toUpperCase();
+        String ten = fullName.substring(lastSpaceIndex + 1).toUpperCase();
+        String tenDem = fullName.substring(firstSpaceIndex + 1, lastSpaceIndex);
 
+        System.out.println("Họ: " + ho);
+        System.out.println("Tên đệm: " + tenDem);
+        System.out.println("Tên: " + ten);
+    }
 }
