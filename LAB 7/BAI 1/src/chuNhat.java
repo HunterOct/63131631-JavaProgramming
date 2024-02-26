c9a81ad619bc1fb43040b29aa20ee77354d6490d
Hữu Tường <hunter.19oct@gmail.com>
2024-02-26 17:41:44 +0700

    Update chuNhat.java

--- LAB 7/BAI 1/src/chuNhat.java
+++ LAB 7/BAI 1/src/chuNhat.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class chuNhat {
-
+	
+	public static double getChuVi(double rong ,double dai) {
+		return (dai+rong)/2 ;
+	}
+	public static double getDienTich(double rong,double dai) {
+		return dai * rong;
+	}
+	public static void xuat(double rong,double dai) {
+		System.out.printf("\nDiện Tích Hình Chữ Nhật:" + getDienTich(rong,dai));
+		System.out.printf("\nChu Vi Hình Chữ Nhật:"+ getChuVi(rong,dai));
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		double rong;
+		double dai;
+		System.out.print("Nhập Chiều Rộng:");
+		rong = scanner.nextDouble();
+		System.out.print("Nhập Chiều Dài:");
+		dai = scanner.nextDouble();
+		getDienTich(rong,dai);
+		getChuVi(rong,dai);
+		xuat(rong,dai);
 
 	}
 
