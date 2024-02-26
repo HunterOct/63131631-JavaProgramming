e0bc4be369b944583d1de37c706e67997b45826b
Hữu Tường <hunter.19oct@gmail.com>
2024-02-26 18:03:18 +0700

    Update Main.java

--- LAB 7/BAI 1/src/Main.java
+++ LAB 7/BAI 1/src/Main.java
@@ -9,7 +9,7 @@ class ChuNhat{
 		this.rong = rong;
 	}
 	public  double getChuVi() {
-		return (dai+rong)/2 ;
+		return (dai+rong)*2 ;
 	}
 	public  double getDienTich() {
 		return dai * rong;
@@ -19,18 +19,32 @@ class ChuNhat{
 		System.out.printf("\nChu Vi Hình Chữ Nhật:"+ getChuVi());
 	}
 }
-
+class vuong extends ChuNhat{
+	public vuong(double canh) {
+		super(canh,canh);
+	}
+	@Override
+	public void xuat() {
+		System.out.printf("\nChu Vi Hình Vuông:"+getChuVi());
+		System.out.printf("\nDiện Tích Hình Vuông:"+getDienTich());
+	}
+}
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		double rong;
 		double dai;
+		double canh;
 		System.out.print("Nhập Chiều Rộng:");
 		rong = scanner.nextDouble();
 		System.out.print("Nhập Chiều Dài:");
 		dai = scanner.nextDouble();
+		System.out.print("Nhập cạnh Hình Vuông:");
+		canh = scanner.nextDouble();
 		ChuNhat cn = new ChuNhat(rong,dai);
+		ChuNhat vuong = new vuong(canh);
 		cn.xuat();
+		vuong.xuat();
 	}
 }
 
